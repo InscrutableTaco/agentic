94cce9917b09044257f74911353ba0d864a2a58f
Jonathan Gibson <jonathan.w.d.gibson@gmail.com>
2025-06-11 17:38:09 -0700

    basic input and response

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+load_dotenv()
+
+def main():
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    prompt = input("Please enter a prompt: ")
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=prompt
+    )
+
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    print(f"{response.text}\nPrompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}\n")
+
+main()
