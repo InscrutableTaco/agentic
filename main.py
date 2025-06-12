6a58240ca998e1c969f9f5c3445992bfbd99c484
Jonathan Gibson <jonathan.w.d.gibson@gmail.com>
2025-06-12 16:37:13 -0700

    store prompt in a list

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
-import os
-from dotenv import load_dotenv
-from google import genai
 import sys
+import os
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
 
-load_dotenv()
 
 def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    prompt = sys.argv
+    user_prompt = " ".join(args)
 
-    if len(prompt) < 2:
-        raise Exception("Please provide a prompt")
-        #sys.exit()
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
-        model = 'gemini-2.0-flash-001', contents=prompt
+        model="gemini-2.0-flash-001",
+        contents=messages,
     )
+    print("Response:")
+    print(response.text)
 
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
 
-    print(f"{response.text}\nPrompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}\n")
-
-main()
+if __name__ == "__main__":
+    main()
