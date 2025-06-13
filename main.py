b79f4b884541c558e9ce0e8e4a64969546fc60d0
Jonathan Gibson <jonathan.w.d.gibson@gmail.com>
2025-06-12 17:30:31 -0700

    verbose parameter

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 
-
 def main():
     load_dotenv()
 
@@ -15,27 +14,41 @@ def main():
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
+
+    verbose = False
+    
+    if "--verbose" in args:
+        verbose = True
+        args.remove("--verbose")
+
     user_prompt = " ".join(args)
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    user_prompt = " ".join(args)
-
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)
+    generate_content(client, messages, user_prompt, verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, user_prompt, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
-    print("Response:")
-    print(response.text)
+
+    if verbose:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    else:
+        print("Response:")
+        print(response.text)
 
 
 if __name__ == "__main__":
